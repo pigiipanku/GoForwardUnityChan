de10e69a07336e5e811b26546894fd94022d0df8
早川　尚吾 <pigiipanku@gmail.com>
2017-09-22 01:11:02 +0900

    GoforwordUnitychan
    課題はできた気がしたが、できていなかった。

---
+++ Assets/Cubesound.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cubesound : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnCollisionEnter(Collider other)
+    {
+        if (other.gameObject.tag == "GroundTag"|| other.gameObject.tag == "BlockTag")
+        {
+            GameObject.Find("CubeGenerator").GetComponent<AudioSource>().Play();
+        }
+    }
+}
---
+++ Assets/UnityChanCotroller.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnityChanCotroller : MonoBehaviour {
+
+    // Use this for initialization
+    Animator animator;
+    Rigidbody2D rigid2D;
+    private float groundLevel = -3.0f;
+    private float dump = 0.8f;
+    float jumpVelocity=20;
+    private float deadLine = -9;
+	void Start () {
+        this.animator = GetComponent<Animator>();
+        this.rigid2D = GetComponent<Rigidbody2D>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.animator.SetFloat("Horizontal", 1);
+        bool isGround = (transform.position.y > this.groundLevel) ? false : true;
+        this.animator.SetBool("isGround", isGround);
+        GetComponent<AudioSource>().volume = (isGround) ? 1: 0;
+        if (Input.GetMouseButtonDown(0) && isGround)
+        {
+            this.rigid2D.velocity = new Vector2(0, this.jumpVelocity);
+        }
+        if (Input.GetMouseButton(0) == false)
+        {
+            if (this.rigid2D.velocity.y > 0)
+            {
+                this.rigid2D.velocity *= this.dump;
+            }
+        }
+        if (transform.position.x < this.deadLine) {
+            GameObject.Find("Canvas").GetComponent<UIcontroller>().GameOver();
+            Destroy(gameObject);
+        }
+	}
+}
